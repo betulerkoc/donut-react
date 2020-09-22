6bcd2b74b54b0a73514428df27c6d7f85321d379
Ammar <ammar.n.sammour@gmail.com>
2020-09-22 16:34:25 +0300

    build a form to add more donuts to the state

--- src/container/donuts/index.js
+++ src/container/donuts/index.js
@@ -10,6 +10,21 @@ export default function Donuts() {
       recommended: false,
     },
   ]);
+  const [userDonut, setUserDonut] = React.useState({
+    name: "",
+    flavor: "",
+    topping: [],
+    recommended: false,
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setDonutsMenu([...donutsMenu, userDonut])
+    console.log(donutsMenu);
+  }
+  const handleInputChange = (e, key) => {
+    setUserDonut({ ...userDonut, [key]: e.target.value });
+    console.log(userDonut);
+  };
 
   const renderDonuts = () => {
     return donutsMenu.map((donut) => <Donut {...donut} key={donut.name} />);
@@ -18,6 +33,16 @@ export default function Donuts() {
   return (
     <div>
       {/* Your Form Should be here */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="donut name" onChange={(e) => handleInputChange(e, "name")}/>
+        <select name="flavor" id="flavor" onChange={(e) => handleInputChange(e, "flavor")}>
+          <option value="chocolate">chocolate</option>
+          <option value="vanila">vanila</option>
+          <option value="strawberry">strawberry</option>
+        </select>
+        <input type="submit"/>
+      </form>
+      <br/>
       {renderDonuts()}
     </div>
   );
